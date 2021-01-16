139c1b7e7f733eafa960605675ef0ff8a29ee164
Tsvetan Borisov <tsborissov@gmail.com>
2021-01-16 10:33:32 +0200

    db

---
+++ CRUD Exercises/CRUD Exercises/Queries for Geography Database.sql
@@ -0,0 +1,18 @@
+USE Geography
+
+SELECT PeakName
+	FROM Peaks
+	ORDER BY PeakName
+
+SELECT TOP(30) CountryName, Population
+	FROM Countries
+	WHERE ContinentCode = 'EU'
+	ORDER BY [Population] DESC
+
+SELECT CountryName, CountryCode,
+	CASE
+		WHEN CurrencyCode = 'EUR' THEN 'Euro'
+		ELSE 'Not Euro'
+	END AS [Currency]
+	FROM Countries
+	ORDER BY CountryName ASC
